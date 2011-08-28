866d0241f77be469f7c5d7ef06e7a3ced37f2fa3
Fabricio C Zuardi <fabricio@fabricio.org>
2011-08-28 17:39:08 -0300

    images cicling and proper label faces

--- experiments/classificador/tag_along_client.js
+++ experiments/classificador/tag_along_client.js
@@ -1,5 +1,75 @@
 var config = {
   slots: 5,
+  photos:[
+    {url:'http://farm3.static.flickr.com/2538/4165413563_f0d0c161c9_m.jpg'},
+    {url:'http://farm4.static.flickr.com/3441/3923119254_73253efea2_m.jpg'},
+    {url:'http://farm7.static.flickr.com/6193/6080825482_b66e80b669_m.jpg'},
+    {url:'http://farm7.static.flickr.com/6206/6085626220_860a6a59cc.jpg'  },
+    {url:'http://farm7.static.flickr.com/6088/6087833418_5b6d53426d.jpg'  },
+    {url:'http://farm2.static.flickr.com/1281/1294684643_eda0c0e755.jpg'  },
+    {url:'http://farm7.static.flickr.com/6011/6009886158_26a80585e6.jpg'  },
+    {url:'http://farm7.static.flickr.com/6070/6086960682_48fa068baf.jpg'  },
+    {url:'http://farm7.static.flickr.com/6083/6086618107_ea27a58ee8.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm3.static.flickr.com/2538/4165413563_f0d0c161c9_m.jpg'},
+    {url:'http://farm4.static.flickr.com/3441/3923119254_73253efea2_m.jpg'},
+    {url:'http://farm7.static.flickr.com/6193/6080825482_b66e80b669_m.jpg'},
+    {url:'http://farm7.static.flickr.com/6206/6085626220_860a6a59cc.jpg'  },
+    {url:'http://farm7.static.flickr.com/6088/6087833418_5b6d53426d.jpg'  },
+    {url:'http://farm2.static.flickr.com/1281/1294684643_eda0c0e755.jpg'  },
+    {url:'http://farm7.static.flickr.com/6011/6009886158_26a80585e6.jpg'  },
+    {url:'http://farm7.static.flickr.com/6070/6086960682_48fa068baf.jpg'  },
+    {url:'http://farm7.static.flickr.com/6083/6086618107_ea27a58ee8.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm3.static.flickr.com/2538/4165413563_f0d0c161c9_m.jpg'},
+    {url:'http://farm4.static.flickr.com/3441/3923119254_73253efea2_m.jpg'},
+    {url:'http://farm7.static.flickr.com/6193/6080825482_b66e80b669_m.jpg'},
+    {url:'http://farm7.static.flickr.com/6206/6085626220_860a6a59cc.jpg'  },
+    {url:'http://farm7.static.flickr.com/6088/6087833418_5b6d53426d.jpg'  },
+    {url:'http://farm2.static.flickr.com/1281/1294684643_eda0c0e755.jpg'  },
+    {url:'http://farm7.static.flickr.com/6011/6009886158_26a80585e6.jpg'  },
+    {url:'http://farm7.static.flickr.com/6070/6086960682_48fa068baf.jpg'  },
+    {url:'http://farm7.static.flickr.com/6083/6086618107_ea27a58ee8.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+    {url:'http://farm7.static.flickr.com/6090/6079143531_083839517d.jpg'  },
+    {url:'http://farm6.static.flickr.com/5066/5673926509_32435ab516.jpg'  },
+    {url:'http://farm1.static.flickr.com/115/304810883_807dd2c83b.jpg'    },
+    {url:'http://farm2.static.flickr.com/1370/5106564165_5f20388d08.jpg'  },
+  ],
+  bucketnames: ['foo', 'bar', 'baz', 'foobar', 'wombat'],
   // //http://www.colourlovers.com/palette/92095/Giant_Goldfish
   // colors: [
   //   'rgb(105,210,231)',
@@ -32,22 +102,22 @@ var config = {
   //  'rgb(198,229,217)',
   //  'rgb(214,129,137)'
   // ]
-  // //http://www.colourlovers.com/palette/90734/Newly_Risen_Moon
-  // colors: [
-  //  'rgb(238,230,171)',
-  //  'rgb(197,188,142)',
-  //  'rgb(105,103,88)',
-  //  'rgb(69,72,75)',
-  //  'rgb(54,57,59)'
-  // ]
-  //http://www.colourlovers.com/palette/69661/The_Sketch
+  //http://www.colourlovers.com/palette/90734/Newly_Risen_Moon
   colors: [
-  'rgb(173,192,180)',
-   'rgb(190,214,199)',
-   'rgb(138,126,102)',
-   'rgb(167,155,131)',
-   'rgb(187,178,161)'
+   'rgb(238,230,171)',
+   'rgb(197,188,142)',
+   'rgb(105,103,88)',
+   'rgb(69,72,75)',
+   'rgb(54,57,59)'
   ]
+  // //http://www.colourlovers.com/palette/69661/The_Sketch
+  // colors: [
+  // 'rgb(173,192,180)',
+  //  'rgb(190,214,199)',
+  //  'rgb(138,126,102)',
+  //  'rgb(167,155,131)',
+  //  'rgb(187,178,161)'
+  // ]
   };
 var throbber_pulse_interval = 0,
     touch_startX = 0,
@@ -55,11 +125,31 @@ var throbber_pulse_interval = 0,
     touch_lastX = 0,
     touch_lastY = 0,
     touch_oneFingerOnly = false,
-    wedgeColors = [];
+    fatia = 0,
+    wedgeColors = [],
+    slotAtivo = 1,
+    firstPhotoPos = 0;
+
+function trocaLabelSeNecessario(){
+  var temhover = false;
+  for (i=0; i<config.slots; i++){
+    if (document.getElementById('wedge'+i).getAttribute('class').length > 0){
+      temhover = true;
+    }
+  }
+  if(!temhover){
+    // document.getElementById('photo-label').style.display = 'none';
+    document.getElementById('photo-label-text').innerHTML = 'skip';
+    document.getElementById('photo-label').className = 'skip';
+    // console.log('esconde');
+  }
+}
+
 
 function resetaCoresFatias(){
   for (i=0; i<config.slots; i++){
     document.getElementById('wedge'+i).setAttribute('fill', 'rgb('+wedgeColors[i][0]+','+wedgeColors[i][1]+','+wedgeColors[i][2]+')');  
+    document.getElementById('wedge'+i).setAttribute('class', '');
   }
 }
 function extractRGB(s){
@@ -71,13 +161,38 @@ function extractRGB(s){
 }
 function overWedge(el){
   var rgb = extractRGB(el.getAttribute('fill')),
+      wedge_index = parseInt(el.id[el.id.length-1],10),
       boost = 20;
   rgb[0] = Math.min(255,rgb[0]+boost);
   rgb[1] = Math.min(255,rgb[1]+boost);
   rgb[2] = Math.min(255,rgb[2]+boost);
   el.setAttribute('fill', 'rgb('+rgb[0]+','+rgb[1]+','+rgb[2]+')');
+  el.setAttribute('class', 'selecionado');
+  document.getElementById('photo-label').style.display = 'block';
+  //TROCAR PELO BUCKETNAME
+  document.getElementById('photo-label-text').innerHTML = config.bucketnames[wedge_index];
+  document.getElementById('photo-label').className = '';
+  // console.log('mostra');
+  document.getElementById('label-color').style.backgroundColor = 'rgb('+rgb[0]+','+rgb[1]+','+rgb[2]+')';
+}
+function getLastDigit(s){
+  return parseInt(s[s.length-1],10);
+}
+function checaToqueEmAtivo(t){
+  var noativo = true;
+  if(t.nodeName.toLowerCase() != 'img'){
+    noativo = false;
+  }
+  var slot_clicado = getLastDigit(t.id);
+  if (slot_clicado !== slotAtivo){
+    noativo = false;
+  }
+  return noativo;
 }
 function touchstart(event){
+  if (!checaToqueEmAtivo(event.target)){
+    return false;
+  }
 	event.preventDefault();
 	if(event.touches.length == 1){ // Only deal with one finger
 		touch_oneFingerOnly = true;
@@ -91,6 +206,9 @@ function touchstart(event){
 }
 }
 function touchmove(event){
+  if (!checaToqueEmAtivo(event.target)){
+    return false;
+  }
 	if(event.touches.length == 1){ // Only deal with one finger
     var touch = event.touches[0]; // Get the information for finger #1
 		//atualiza as variaveis enquanto mudar
@@ -101,19 +219,82 @@ function touchmove(event){
 		var deltaY = touch_lastY - touch_startY;
 		var angle = Math.atan2(deltaY,deltaX)/(Math.PI/180);
 		var step = 180/config.slots;
-		var fatia = Math.ceil(angle / step);
+		fatia = Math.ceil(angle / step);
 		resetaCoresFatias();
-		overWedge(document.getElementById('wedge'+(fatia-1)));
 		document.getElementById('score-left').innerHTML = fatia;
+    document.getElementById('photo-label').style.display = 'block';
+		var wedge = document.getElementById('wedge'+(fatia-1));
+		if (wedge){
+      overWedge(wedge);
+    } else {
+      trocaLabelSeNecessario();
+		}
   }
 }
+function loadFirstPhotos(){
+  for(i=1;i<=3;i++){
+    document.getElementById('photo-source-'+i).setAttribute('src', '');
+    document.getElementById('photo-source-'+i).setAttribute('src', config.photos[firstPhotoPos+i-1].url);
+  }
+}
+function ciclePhotos(){
+  firstPhotoPos ++;
+  for(i=1;i<=3;i++){
+    var slot = document.getElementById('photo-slot-'+i);
+    var imgsrc = document.getElementById('photo-source-'+i);
+    var position = getLastDigit(slot.className);
+    var new_position = position-1;
+    if (new_position < 1) {
+      new_position = 3;
+      //new src here
+      imgsrc.setAttribute('src','');
+      imgsrc.setAttribute('src',config.photos[firstPhotoPos+2].url);
+      // console.log(firstPhotoPos+' new src:'+config.photos[firstPhotoPos+2].url);
+    }
+    slot.className = "photo p"+new_position;    
+    if (new_position == 1){
+      slotAtivo = i;
+      var pl = document.getElementById('photo-label');
+      var tempnode = pl.cloneNode(true);
+      var throwawayNode = pl.parentNode.removeChild(pl);
+      slot.appendChild(tempnode);
+      var imageWidth = imgsrc.offsetWidth;
+      console.log(i + ' ' +imageWidth);
+      tempnode.style.marginLeft = -13 -(240 - imageWidth)/2 + 'px';
+    }
+  }
+  firstPhotoPos ++;
+}
+function getNextPhoto(){
+  document.getElementById('photo-label').style.display = 'none';
+  ciclePhotos();
+}
+function sendLabel(label){
+  console.log('Send:'+label);
+}
+function evaluateLabeling(ev){
+  var wedge_index = -1;
+  if(ev.target.nodeName.toLowerCase() == 'path'){
+    wedge_index = parseInt(ev.target.id[ev.target.id.length-1],10);
+  }else{
+    wedge_index = fatia -1;
+  }
+  if(wedge_index >= 0){
+    sendLabel(config.bucketnames[wedge_index]);
+  }
+  getNextPhoto();
+}
 function touchend(event){
+  if (!checaToqueEmAtivo(event.target)){
+    return false;
+  }
 	if(touch_oneFingerOnly){
     // var deltaX = touch_lastX - touch_startX;
     // var deltaY = touch_lastY - touch_startY;
     // var angle = Math.atan2(deltaY,deltaX)/(Math.PI/180);
     // console.log('ended with deltaX: ' + deltaX + " and deltaY: " + deltaY + " atan2: " + angle);
     // document.getElementById('score-left').innerHTML = Math.round(angle);
+    evaluateLabeling(event);
 	}
 }
 function touchcancel(event){
@@ -204,10 +385,10 @@ function drawBuckets(slots){
   var color_rnd = config.colors[Math.round(Math.random())+2];
   var svg_prefix = "<svg id=\"base\" fill=\"none\" stroke=\"none\" width=\"2500\" height=\"5000\"><g><path transform=\"translate(0, 2500)\" d=\"M3.061616997868383e-13,-5000A5000,5000 0 0,1 ";
   var svg_paths = [
-    "5000,0L0,0Z\" fill=\""+ config.colors[4] +"\" fill-rule=\"evenodd\"  id=\"wedge0\"/><path id=\"wedge1\" transform=\"translate(0, 2500)\" d=\"M5000,0A5000,5000 0 0,1 3.061616997868383e-13,5000L0,0Z\" fill=\""+ color_rnd +"\"",
-    "4330.127018922193,-2500L0,0Z\" fill=\""+ config.colors[4] +"\" fill-rule=\"evenodd\" id=\"wedge0\"/><path id=\"wedge1\" transform=\"translate(0, 2500)\" d=\"M4330.127018922193,-2500A5000,5000 0 0,1 4330.127018922193,2499.999999999999L0,0Z\" fill=\""+ config.colors[3] +"\" fill-rule=\"evenodd\"/><path id=\"wedge2\" transform=\"translate(0, 2500)\" d=\"M4330.127018922193,2499.999999999999A5000,5000 0 0,1 1.416384724411995e-12,5000L0,0Z\" fill=\""+ config.colors[2] +"\"",
-    "3535.533905932738,-3535.5339059327375L0,0Z\" fill=\""+ config.colors[4] +"\" fill-rule=\"evenodd\" id=\"wedge0\"/><path id=\"wedge1\" transform=\"translate(0, 2500)\" d=\"M3535.533905932738,-3535.5339059327375A5000,5000 0 0,1 5000,0L0,0Z\" fill=\""+ config.colors[3] +"\" fill-rule=\"evenodd\"/><path id=\"wedge2\" transform=\"translate(0, 2500)\" d=\"M5000,0A5000,5000 0 0,1 3535.533905932738,3535.5339059327375L0,0Z\" fill=\""+ config.colors[0] +"\" fill-rule=\"evenodd\"/><path id=\"wedge3\" transform=\"translate(0, 2500)\" d=\"M3535.533905932738,3535.5339059327375A5000,5000 0 0,1 3.061616997868383e-13,5000L0,0Z\" fill=\""+ config.colors[2] +"\"",
-    "2938.9262614623663,-4045.0849718747368L0,0Z\" fill=\""+ config.colors[4] +"\" fill-rule=\"evenodd\" id=\"wedge0\"/><path id=\"wedge1\" transform=\"translate(0, 2500)\" d=\"M2938.9262614623663,-4045.0849718747368A5000,5000 0 0,1 4755.282581475768,-1545.084971874737L0,0Z\" fill=\""+ config.colors[0] +"\" fill-rule=\"evenodd\"/><path id=\"wedge2\" transform=\"translate(0, 2500)\" d=\"M4755.282581475768,-1545.084971874737A5000,5000 0 0,1 4755.282581475768,1545.084971874737L0,0Z\" fill=\""+ config.colors[3] +"\" fill-rule=\"evenodd\"/><path id=\"wedge3\" transform=\"translate(0, 2500)\" d=\"M4755.282581475768,1545.084971874737A5000,5000 0 0,1 2938.9262614623663,4045.0849718747368L0,0Z\" fill=\""+ config.colors[1] +"\" fill-rule=\"evenodd\"/><path id=\"wedge4\" transform=\"translate(0, 2500)\" d=\"M2938.9262614623663,4045.0849718747368A5000,5000 0 0,1 3.061616997868383e-13,5000L0,0Z\" fill=\""+ config.colors[2] +"\""
+    "5000,0L0,0Z\" fill=\""+ config.colors[4] +"\" class=\"\" fill-rule=\"evenodd\"  id=\"wedge0\"/><path id=\"wedge1\" transform=\"translate(0, 2500)\" d=\"M5000,0A5000,5000 0 0,1 3.061616997868383e-13,5000L0,0Z\" class=\"\" fill=\""+ color_rnd +"\"",
+    "4330.127018922193,-2500L0,0Z\" class=\"\" fill=\""+ config.colors[4] +"\" fill-rule=\"evenodd\" id=\"wedge0\"/><path id=\"wedge1\" transform=\"translate(0, 2500)\" d=\"M4330.127018922193,-2500A5000,5000 0 0,1 4330.127018922193,2499.999999999999L0,0Z\" class=\"\" fill=\""+ config.colors[3] +"\" fill-rule=\"evenodd\"/><path id=\"wedge2\" transform=\"translate(0, 2500)\" d=\"M4330.127018922193,2499.999999999999A5000,5000 0 0,1 1.416384724411995e-12,5000L0,0Z\" class=\"\" fill=\""+ config.colors[2] +"\"",
+    "3535.533905932738,-3535.5339059327375L0,0Z\" class=\"\" fill=\""+ config.colors[4] +"\" fill-rule=\"evenodd\" id=\"wedge0\"/><path id=\"wedge1\" transform=\"translate(0, 2500)\" d=\"M3535.533905932738,-3535.5339059327375A5000,5000 0 0,1 5000,0L0,0Z\" class=\"\" fill=\""+ config.colors[3] +"\" fill-rule=\"evenodd\"/><path id=\"wedge2\" transform=\"translate(0, 2500)\" d=\"M5000,0A5000,5000 0 0,1 3535.533905932738,3535.5339059327375L0,0Z\" class=\"\" fill=\""+ config.colors[0] +"\" fill-rule=\"evenodd\"/><path id=\"wedge3\" transform=\"translate(0, 2500)\" d=\"M3535.533905932738,3535.5339059327375A5000,5000 0 0,1 3.061616997868383e-13,5000L0,0Z\" class=\"\" fill=\""+ config.colors[2] +"\"",
+    "2938.9262614623663,-4045.0849718747368L0,0Z\" class=\"\" fill=\""+ config.colors[4] +"\" fill-rule=\"evenodd\" id=\"wedge0\"/><path id=\"wedge1\" transform=\"translate(0, 2500)\" d=\"M2938.9262614623663,-4045.0849718747368A5000,5000 0 0,1 4755.282581475768,-1545.084971874737L0,0Z\" class=\"\" fill=\""+ config.colors[0] +"\" fill-rule=\"evenodd\"/><path id=\"wedge2\" transform=\"translate(0, 2500)\" d=\"M4755.282581475768,-1545.084971874737A5000,5000 0 0,1 4755.282581475768,1545.084971874737L0,0Z\" class=\"\" fill=\""+ config.colors[3] +"\" fill-rule=\"evenodd\"/><path id=\"wedge3\" transform=\"translate(0, 2500)\" d=\"M4755.282581475768,1545.084971874737A5000,5000 0 0,1 2938.9262614623663,4045.0849718747368L0,0Z\" class=\"\" fill=\""+ config.colors[1] +"\" fill-rule=\"evenodd\"/><path id=\"wedge4\" transform=\"translate(0, 2500)\" d=\"M2938.9262614623663,4045.0849718747368A5000,5000 0 0,1 3.061616997868383e-13,5000L0,0Z\" class=\"\" fill=\""+ config.colors[2] +"\""
   ];
   var svg_sufix = " fill-rule=\"evenodd\"/></g></svg>";
   document.getElementById('bucket-background').innerHTML = svg_prefix + svg_paths[slots-2] + svg_sufix;
@@ -225,20 +406,28 @@ function wedgeHover(ev){
 }
 function wedgeOut(ev){
   var wedge_index = parseInt(ev.target.id.charAt(ev.target.id.length-1),10);
-  ev.target.setAttribute('fill', 'rgb('+wedgeColors[wedge_index][0]+','+wedgeColors[wedge_index][1]+','+wedgeColors[wedge_index][2]+')');  
+  ev.target.setAttribute('fill', 'rgb('+wedgeColors[wedge_index][0]+','+wedgeColors[wedge_index][1]+','+wedgeColors[wedge_index][2]+')');
+  ev.target.setAttribute('class', '');
+  setTimeout(trocaLabelSeNecessario, 2);
+}
+function scoreClicked(){
+  var isiPad = navigator.userAgent.match(/iPad/i) !== null;
+  if(! isiPad){
+    getNextPhoto();
+  }
 }
 //dom ready
 function pageLoaded(){
   windowResized();
   drawBuckets(config.slots);
   window.addEventListener('resize', windowResized, true);
-  console.log(config.slots);
   for (i=0; i<config.slots; i++){
-    console.log(document.getElementById('wedge'+i));
     var wedge = document.getElementById('wedge'+i);
     wedgeColors.push(extractRGB(wedge.getAttribute('fill')));
     wedge.addEventListener('mouseover', wedgeHover, true);
     wedge.addEventListener('mouseout', wedgeOut, true);
+    wedge.addEventListener('click', evaluateLabeling, true);
   }
-  console.log(wedgeColors);
+  document.getElementById('score').addEventListener('click', scoreClicked, true);
+  loadFirstPhotos();
 }
