9a1234aa9f2edbdf187c08a4b7859c5cdcee0d9f
Daniel Quirino Oliveira <danielqo@gmail.com>
2011-08-28 15:21:16 -0300

    faye port being blocked in joyent smartmachine... now binding to express server

--- server.js
+++ server.js
@@ -166,7 +166,8 @@ app.get('/quests', function(req, res){
 
 
 // Binding and starting up...
+pubsub.attach(app);
 app.listen(WEBSERVER_PORT);
-pubsub.listen(QuestUtils.getFayePort());
+// pubsub.listen(QuestUtils.getFayePort());
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-console.log("Faye server listening on port %d in %s mode", QuestUtils.getFayePort(), app.settings.env);
+// console.log("Faye server listening on port %d in %s mode", QuestUtils.getFayePort(), app.settings.env);
