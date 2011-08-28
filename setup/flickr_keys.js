757c43471f06fdf3875df9b4a9703a1245d2cc1d
Daniel Quirino Oliveira <danielqo@gmail.com>
2011-08-27 23:04:52 -0300

    adding keys

---
+++ setup/flickr_keys.js
@@ -0,0 +1,12 @@
+//edit this file and rename it to flickr_keys.js
+flickr_setup = {
+  'key': '9e26aa58e637506c7b6979556f9357d7',
+  'secret': '67a039c6348bbe14'
+	// 'key': '35b73b32868bcba8e64f297bf8521726',
+	// 'secret': '8bcc8e9f5d571a63'
+	// 'key': 'a3d4c9c1efb9f40d7c583b36678db78b',
+	// 'secret': '0d72bee85c3b095c'
+	// 'key': 'ffcb5dfa3a22936f4e5134e53e7f4c03',
+	// 'secret': '8175a23d00b394ba'
+};
+exports.setup = flickr_setup;
---
+++ setup/nko.js
@@ -0,0 +1,5 @@
+//edit this file and rename it to nko.js
+nko_setup = {
+  'secret': '8XopnAwJ5kmbKTO2'
+};
+exports.setup = nko_setup;
